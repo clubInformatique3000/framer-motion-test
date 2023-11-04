d144e700188ab34fc098a1c5f4812dab271f20c8
Mickael Lespinasse <mickael.lespinasse@sogeti.com>
2023-11-04 21:32:24 +0100

    feat: create square + center

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <div className="green-div square">
+        <div className="transparent-div square">
+          <div className="blue-div square"></div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
